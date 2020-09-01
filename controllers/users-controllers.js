271e2b9f821cc1b7857dfcb988644f1f92d0e424
Andrew <andikk@mail.ru>
2020-09-01 09:39:52 +0300

    token generation

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -2,6 +2,7 @@ const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const HttpError = require('../models/http-error');
+const jwt = require('jsonwebtoken');
 
 const getUsers = async (req, res, next) => {
   let users;
@@ -62,7 +63,24 @@ const signup  = async (req, res, next) => {
     const error = new HttpError('Creating user failed', 500);
     return next(error);
   }
-  res.status(201).json({user:createdUser.toObject({getters: true})});
+
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        userId: createdUser.id,
+        email: createdUser.email
+      },
+      'supersecret_dont_share',
+      {expiresIn: '1h'}
+    );
+
+  } catch (err) {
+    const error = new HttpError('Creating user failed', 500);
+    return next(error);
+  }
+
+  res.status(201).json({userId: createdUser.id, email: createdUser.email, token: token});
 };
 
 const login = async (req, res, next) => {
@@ -94,7 +112,24 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  res.json({message: 'Logged in!', user: existingUser.toObject({getters: true})});
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        userId: existingUser.id,
+        email: existingUser.email
+      },
+      'supersecret_dont_share',
+      {expiresIn: '1h'}
+    );
+
+  } catch (err) {
+    const error = new HttpError('Creating user failed', 500);
+    return next(error);
+  }
+
+
+  res.json({userId: createdUser.id, email: createdUser.email, token: token});
 
 };
 
